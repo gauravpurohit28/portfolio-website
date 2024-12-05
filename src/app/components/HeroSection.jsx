fc79e62e8a9c6cd7c8eb6ce7eff1421030af1f5f
Gaurav Purohit <grajpurohittt@gmail.com>
2024-12-05 20:43:40 +0530

    Initial commit

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1 className='text-white mb-4 text-6xl font-extrabold'>
+        Hello, I'm Gaurav
+      </h1>
+      <p className='text-[#ADB7BE]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam inventore id voluptatem iure atque, illo nisi et vitae eligendi autem nesciunt omnis amet quo! Quas exercitationem facere nobis fugiat delectus!</p>
+     
+    </section>
+  )
+}
+
+export default HeroSection

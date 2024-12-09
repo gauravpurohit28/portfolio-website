5222b8a79668dc9889b1b1b515ed4a5d7a48f047
Gaurav Purohit <grajpurohittt@gmail.com>
2024-12-09 23:21:40 +0530

    menubar overlaangesy ch

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/navbar";
+import MenuOverlay from "./components/MenuOverlay";
 import { Container } from "postcss";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar/>
       <div className="container mx-auto px-12 py-4">
         <HeroSection/>
+        <MenuOverlay/>
       </div>
     </main>
   );
